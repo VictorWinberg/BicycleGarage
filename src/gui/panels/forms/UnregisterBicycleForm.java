dbe1e0856fe0c5e885a19fe84920c871696a8d14
Anton <Anton@ANTON>
2015-05-18 16:38:39 +0200

    Registrera och avregistrera cykel

--- src/gui/panels/forms/UnregisterBicycleForm.java
+++ src/gui/panels/forms/UnregisterBicycleForm.java
@@ -1,29 +1,33 @@
 package gui.panels.forms;
 
+import interfaces.Database;
 import main.BicycleGarageManager;
+import database.Bicycle;
 
 public class UnregisterBicycleForm extends Form {
+	private BicycleGarageManager manager;
 
 	public UnregisterBicycleForm(BicycleGarageManager manager) {
 		super(manager, "Avregistrera cykel");
+		this.manager= manager;
 	}
 
 	@Override
 	public String[] getLabels() {
-		String[] labels = { "Personnummer", "Streckkod", "Mer" };
+		String[] labels = { "Streckkod" };
 		return labels;
 	}
 
 	@Override
 	public int[] getWidths() {
-		int[] widths = { 11, 4, 2 };
+		int[] widths = { 11 };
 		return widths;
 	}
 
 	@Override
 	public void action(String[] fields) {
-		System.out.println("Cykelavregistrering:");
-		for (int i = 0; i < fields.length; i++)
-			System.out.println(fields[i]);
+		Database db = manager.getDB();
+		Bicycle bc = db.getBicycle(fields[0]);
+		db.deleteBicycle(bc);
 	}
 }
