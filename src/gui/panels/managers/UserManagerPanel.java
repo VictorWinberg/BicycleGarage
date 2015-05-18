04efdf2581ac8ef3a839258595fd7be7c7847e3d
Anton <Anton@ANTON>
2015-05-18 12:30:11 +0200

    Beautiful panel

--- src/gui/panels/managers/UserManagerPanel.java
+++ src/gui/panels/managers/UserManagerPanel.java
@@ -14,9 +14,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.SoftBevelBorder;
 
+import database.User;
 import main.BicycleGarageManager;
-
-
 
 /**
  * Denna panel öppnas när man klickar på “Användare” i operatörsgränssnittet och
@@ -42,59 +41,35 @@ public class UserManagerPanel extends JPanel {
 		panel.add(new UnregisterUserButton(manager, 1.1));
 		add(panel, BorderLayout.NORTH);
 		Database db = manager.getDB();
-		String[] columnNames = { "First Name", "Last Name", "Sport",
-				"# of Years", "Vegetarian" };
+		String[] columnNames = { "Personummer", "Förnamn", "Efternman", "Mail",
+				"Telefonnummer", "PIN-kod", "Reserverade platser",
+				"Lediga platser" };
 		try {
-	//	if(db!=null){
 			ResultSet users = db.extractUsers();
-		if(users!=null){
-		ResultSetMetaData rmsd = users.getMetaData();
-		users.last();
-		Object [][] data = new ResultSet [users.getRow()][8];
+			ResultSetMetaData rmsd = users.getMetaData();
+			users.last();
+			Object[][] data = new Object[users.getRow()][8];
 			users.beforeFirst();
 			int j = 0;
 			while (users.next()) {
-				for(int i = 0; i < rmsd.getColumnCount(); i ++){
-					data[j][i] = users.getString(i);
-					System.out.println(rmsd.getColumnCount());
-//				new User(users.getString(1), users.getString(2),
-//							users.getString(3), users.getString(4), users.getString(5),
-//							users.getString(6), users.getInt(7), users.getInt(8));
-						}
-				j++;
+				for (int i = 0; i < 6; i++) {
+					data[j][i] = users.getString(i + 1);
 				}
-				JTable table = new JTable(data, columnNames);
-				add(table.getTableHeader(), BorderLayout.CENTER);
-//				table.setEnabled(false);
-//				table.setFillsViewportHeight(true);
-//				table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-				JScrollPane scrollPane = new JScrollPane(table);
-				add(scrollPane, BorderLayout.SOUTH);
-			} 
-		}catch (SQLException e) {
-			System.out.println("SQL Message i getUser: " + e.getMessage());
+				data[j][6] = users.getInt(7);
+				data[j][7] = users.getInt(8);
+				j++;
+			}
+			JTable table = new JTable(data, columnNames);
+			add(table.getTableHeader(), BorderLayout.CENTER);
+			// table.setEnabled(false);
+			// table.setFillsViewportHeight(true);
+			// table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+			JScrollPane scrollPane = new JScrollPane(table);
+			add(scrollPane, BorderLayout.SOUTH);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
-//		Object[][] data = {
-//				{ "Kathy", "Smith", "Snowboarding", new Integer(5),
-//						new Boolean(false) },
-//				{ "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
-//				{ "Sue", "Black", "Knitting", new Integer(2),
-//						new Boolean(false) },
-//				{ "Jane", "White", "Speed reading", new Integer(20),
-//						new Boolean(true) },
-//				{ "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
-//				{ "Sue", "Black", "Knitting", new Integer(2),
-//						new Boolean(false) },
-//				{ "Jane", "White", "Speed reading", new Integer(20),
-//						new Boolean(true) },
-//				{ "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
-//				{ "Sue", "Black", "Knitting", new Integer(2),
-//						new Boolean(false) },
-//				{ "Jane", "White", "Speed reading", new Integer(20),
-//						new Boolean(true) },
-//				{ "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) } };
-		
 	}
 
 }
