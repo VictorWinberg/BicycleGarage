93d81f131caa61d5cb7d740dc056b6851570f459
antongoransson <anton.goransson95@gmail.com>
2015-05-20 15:34:08 +0200

    Mer reservera

--- src/gui/forms/panels/RemoveReservedSlotForm.java
+++ src/gui/forms/panels/RemoveReservedSlotForm.java
@@ -1,6 +1,6 @@
-	package gui.forms.panels;
+package gui.forms.panels;
 
-	import gui.managers.ViewState;
+import gui.managers.ViewState;
 import interfaces.Database;
 
 import javax.swing.JOptionPane;
@@ -8,64 +8,66 @@ import javax.swing.JOptionPane;
 import main.BicycleGarageManager;
 import database.User;
 
+public class RemoveReservedSlotForm extends Form {
+	private BicycleGarageManager manager;
 
-public class RemoveReservedSlotForm extends Form{
-		private BicycleGarageManager manager;
-		
-		public RemoveReservedSlotForm(BicycleGarageManager manager) {
-			super(manager, "Avreservera plats");
-			this.manager = manager;
-		}
+	public RemoveReservedSlotForm(BicycleGarageManager manager) {
+		super(manager, "Avreservera plats");
+		this.manager = manager;
+	}
 
-		@Override
-		public String[] getLabels() {
-			String[] labels = { "Personnummer", "PIN-kod", "Antal platser"};
-			return labels;
-		}
+	@Override
+	public String[] getLabels() {
+		String[] labels = { "Personnummer", "PIN-kod", "Antal platser" };
+		return labels;
+	}
 
-		@Override
-		public int[] getWidths() {
-			int[] widths = { 11, 15, 5};
-			return widths;
-		}
-		
-		private User user;
-		private User userPIN;
-		@Override
-		public boolean check(String[] fields) {
-			Database db = manager.getDB();
-			user = db.getUser(fields[0]);
-			userPIN = db.getUserWithPIN(fields[1]);
-			if(!db.isPNRValid(fields[0])){
-				JOptionPane.showMessageDialog(null, "Personnumret angavs på fel format");
-				return false;
-			}
-			if(user == null){
-				JOptionPane.showMessageDialog(null, "Personnumret är ej kopplat till en användare");
-			return false;
-			}
-			if(userPIN == null){
-				JOptionPane.showMessageDialog(null, "Felaktig PIN-kod");
-				return false;	
-			}
-			if(user.equals(userPIN)){
-				return true;
-			}
+	@Override
+	public int[] getWidths() {
+		int[] widths = { 11, 15, 5 };
+		return widths;
+	}
+
+	private User user;
+	private User userPIN;
+
+	@Override
+	public boolean check(String[] fields) {
+		Database db = manager.getDB();
+		user = db.getUser(fields[0]);
+		userPIN = db.getUserWithPIN(fields[1]);
+		if (!db.isPNRValid(fields[0])) {
+			JOptionPane.showMessageDialog(null,
+					"Personnumret angavs på fel format");
 			return false;
 		}
-
-		@Override
-		public void action(String[] fields) {
-			String nbrOfSpots = fields[2];
-			int spot = Integer.parseInt(nbrOfSpots);
-			if(spot<0){
-				JOptionPane.showMessageDialog(null, "Felaktigt värde på antal platser som ska avreserveras");
-				return;
-			}
-			db.removeReservedSlot(user,spot);
-			JOptionPane.showMessageDialog(null, nbrOfSpots + "plats/er avreserverades");
-			manager.changeState(ViewState.USER_STATE);
+		if (user == null) {
+			JOptionPane.showMessageDialog(null,
+					"Personnumret är ej kopplat till en användare");
+			return false;
 		}
+		if (userPIN == null) {
+			JOptionPane.showMessageDialog(null, "Felaktig PIN-kod");
+			return false;
+		}
+		if (user.equals(userPIN)) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void action(String[] fields) {
+		String nbrOfSpots = fields[2];
+		int spot = Integer.parseInt(nbrOfSpots);
+		if (spot < 0) {
+			JOptionPane.showMessageDialog(null,
+					"Felaktigt värde på antal platser som ska avreserveras");
+			return;
+		}
+		db.removeReservedSlot(user, spot);
+		JOptionPane.showMessageDialog(null, nbrOfSpots
+				+ "plats/er avreserverades");
+		manager.changeState(ViewState.USER_STATE);
+	}
 }
-
-
