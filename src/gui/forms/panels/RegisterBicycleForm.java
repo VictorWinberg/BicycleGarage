9ed8bfa59331382b7ae1a068403540a8698e3acc
VictorWinberg <dat14vwi@student.lu.se>
2015-05-20 14:39:26 +0200

    Database updateUser

--- src/gui/forms/panels/RegisterBicycleForm.java
+++ src/gui/forms/panels/RegisterBicycleForm.java
@@ -1,8 +1,6 @@
 package gui.forms.panels;
 
 import gui.managers.ViewState;
-import interfaces.Database;
-
 import javax.swing.JOptionPane;
 
 import main.BicycleGarageManager;
@@ -31,22 +29,33 @@ public class RegisterBicycleForm extends Form {
 	
 	@Override
 	public boolean check(String[] fields) {
+		if(!db.isPNRValid(fields[0])) {
+			JOptionPane.showMessageDialog(null, "Personnumret angavs på fel format", "Felmeddelande", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
 		User user = db.getUser(fields[0]);
+		if(user == null) {
+			JOptionPane.showMessageDialog(null, "Personnumret är ej kopplat till en användare", "Felmeddelande", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
 		User checkUser = db.getUserWithPIN(fields[1]);
-		if(user.equals(checkUser)){
-			try {
-				bc = db.createBicycle(user);
-				return true;
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, e.getMessage());
-			}
-		} else JOptionPane.showMessageDialog(null, "Felaktig PIN");
-		return false;
+		if(!user.equals(checkUser)){
+			JOptionPane.showMessageDialog(null, "Felaktig PIN", "Felmeddelande", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		try {
+			bc = db.createBicycle(user);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Felmeddelande", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public void action(String[] fields) {
 		db.insertBicycle(bc);
 		manager.changeState(ViewState.BICYCLE_STATE);
+		manager.printBarcode(bc.getOwner().getFirstName() + " har cykel med streckkod: " + bc.getBarcode());
 	}
 }
--- src/gui/forms/panels/UnregisterBicycleForm.java
+++ src/gui/forms/panels/UnregisterBicycleForm.java
@@ -33,15 +33,20 @@ public class UnregisterBicycleForm extends Form {
 	public boolean check(String[] fields) {
 		bc = manager.getDB().getBicycle(fields[0]);
 		if(bc == null) {
-			JOptionPane.showMessageDialog(null, "Cykeln med streckkod " + fields[0] + " finns inte.");
+			JOptionPane.showMessageDialog(null, "Cykeln med streckkod " + fields[0] + " tillhör ej detta garaget");
 			return false;
-		} else
-			return true;
+		}
+		if(bc.isDeposited()) {
+			JOptionPane.showMessageDialog(null, "Cykeln med streckkod " + fields[0] + " är inlämnad i garaget");
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public void action(String[] fields) {
-		manager.getDB().deleteBicycle(bc);
+		db.deleteBicycle(bc);
 		manager.changeState(ViewState.BICYCLE_STATE);
+		JOptionPane.showMessageDialog(null, "Cykeln är borttagen");
 	}
 }
