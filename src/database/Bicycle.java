72d76a16c03a6f226ffeddd62414ca8b7baefb80
p0ff3 <p0ff3@p0ff3-PC>
2015-05-23 16:21:48 +0200

    Kommentarer i Bicyclye

--- src/database/Bicycle.java
+++ src/database/Bicycle.java
@@ -12,27 +12,48 @@ public class Bicycle {
 		this.deposited = deposited;
 	}
 
+	/**
+	 * 
+	 * @return streckkoden i form av en string med 5 tecken
+	 */
 	public String getBarcode() {
 		return barcode;
 	}
 
+	/**
+	 * 
+	 * @return user som är kopplad till streckkoden
+	 */
 	public User getOwner() {
 		return user;
 	}
 
+	/**
+	 * 
+	 * @return true om cykeln är registrerad som inlämnad, false om den inte är
+	 *         det
+	 */
 	public boolean isDeposited() {
 		return deposited;
 	}
 
+	/**
+	 * Ger en string med "personnummer | streckkod | deposited"
+	 */
 	@Override
 	public String toString() {
 		return user.getPersonnr() + " | " + barcode + " | " + deposited;
 	}
 
+	/**
+	 * Registrerar cykeln som inlämnad
+	 */
 	public void deposit() {
 		deposited = true;
 	}
-
+	/**
+	 * Registrerar cykeln som icke inlämnad
+	 */
 	public void withdraw() {
 		deposited = false;
 	}
